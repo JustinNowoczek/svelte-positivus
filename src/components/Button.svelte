10d5413657a20bf6762315a743dd587c8ef44071
Justin N <2tidetwo@gmail.com>
2024-09-04 13:34:47 +0200

    added nav

---
+++ src/components/Button.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let text = ' '
+	export let maxWidth = false
+	export let color: 'light' | 'dark' | 'main' = 'light'
+</script>
+
+<button class={color} class:maxWidth><p>{text}</p></button>
+
+<style>
+	button {
+		text-wrap: nowrap;
+		padding: 20px 35px;
+		border-radius: 14px;
+		width: fit-content;
+
+		&.maxWidth {
+			width: 100%;
+		}
+
+		&.main {
+			background-color: var(--main);
+		}
+
+		&.light {
+			background-color: var(--light);
+			border: 1px solid var(--dark);
+		}
+
+		&.dark {
+			background-color: var(--dark);
+			color: var(--light);
+		}
+	}
+</style>
---
+++ src/sections/Nav.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Icon from '@assets/Icon.svelte'
+	import Button from '@components/Button.svelte'
+
+	const navItems = ['About us', 'Services', 'Use Cases', 'Pricing', 'Blog']
+</script>
+
+<div class="small">
+	<Icon color="dark" fullLogo />
+	<div>
+		{#each navItems as item}
+			<a href={'#' + item.split(' ').join('').toLowerCase()}>{item}</a>
+		{/each}
+		<Button text="Request a Quote" />
+	</div>
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		& div {
+			display: flex;
+			gap: 40px;
+			align-items: center;
+
+			& a {
+				color: var(--dark);
+			}
+		}
+	}
+</style>
