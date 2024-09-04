365a8ab31ce9b8aa2d384bbfc385b66ffcd257b6
Justin N <2tidetwo@gmail.com>
2024-09-04 13:34:12 +0200

    add path alias fixed general styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+	plugins: [svelte()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@sections': path.resolve(__dirname, 'src/sections'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+		},
+	},
 })
+
